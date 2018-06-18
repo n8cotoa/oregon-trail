b2ad71e2c1fa8156d54e21720e951a190c89b947
Nate Cottle and Reese Glasscock <reese@>
2018-06-18 10:01:24 -0700

    add illness generator for characters.

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,22 @@ function Character(name) {
 function Wagon() {
   this.food = 2000;
 }
+// illness generator
+Character.prototype.illnessGenerator = function() {
+  var num = Math.floor(Math.random() * Math.floor(5))
+
+  if (num === 1) {
+    this.illness.push("Dysentery")
+  } else if (num === 2) {
+    this.illness.push("Gonorrhea")
+  } else if (num === 3) {
+    this.illness.push("Yellow Fever")
+  } else if (num === 4) {
+    this.illness.push("Pertussis")
+  } else {
+    this.illness.push("Broken Arm")
+  }
+}
 
 $(document).ready(function(){
   $("#startBTN").click(function(){
