6717bd0875823a53e1e5f3096d9a3166512b265b
Nate Cottle and Reese Glasscock <nate@>
2018-06-18 11:03:28 -0700

    add event grabber with positive, neutral, and negative events.

--- js/scripts.js
+++ js/scripts.js
@@ -34,7 +34,7 @@ Character.prototype.illnessChecker = function() {
     this.health -= 6
   }
 }
-//status checker
+//status adjuster
 Character.prototype.statusAdjuster = function() {
   if (this.health >= 80) {
     this.status = "good"
@@ -44,6 +44,75 @@ Character.prototype.statusAdjuster = function() {
     this.status = "poor"
   } else {
     this.status = "dead"
+  }
+}
+//event grabber
+Wagon.prototype.eventGrabber = function() {
+  var num = Math.floor(Math.random() * Math.floor(100))
+  if (num >= 80) {
+    postiveEvent()
+    //call positive event
+  } else if (num < 80 && num >= 20) {
+    neutralEvent()
+    //call neutral event
+  } else if (num < 20 && num > 5) {
+    //call negative event
+  } else {
+    //call death event
+  }
+}
+
+function positiveEvent() {
+  var num = Math.floor(Math.random() * Math.floor(5))
+  if (num === 1) {
+    //"As you rest by the river, you find some gold"
+    //wagon.money += numbergenerator
+  } else if (num === 2) {
+    //"You come across an abandoned wagon, you find some unspoiled food"
+    // wagon.food += ranNum
+  } else if (num === 3) {
+    //"You found a wounded deer"
+    //wagon.food += ranNum
+  } else if (num === 4) {
+    //As you travel along, you come across a group of suckers. You got some free shit.
+    //give food or gold
+  } else if (num === 5){
+    //You ambush and murder another party. We feast tonight.
+    //wagon.money += 250
+    //wagon.food += 200
+  }
+}
+
+function neutralEvent() {
+  var num = Math.floor(Math.random() * Math.floor(5))
+  if (num === 1) {
+    //One of you ox was pregnant and gave birth. The baby died and she is sad, but continues on.
+  } else if (num === 2) {
+    //You get a letter from home
+  } else if (num === 3) {
+    //Your party finds a small lake and decides to go for a swim
+  } else if (num === 4) {
+    //You find a small bunny and decide to keep it (not as food, what's wrong with you.)
+  } else if (num === 5){
+    //A member of your party explores their sexuality with a neighbor boy.
+  }
+}
+
+function negativeEvent() {
+  var num = Math.floor(Math.random() * Math.floor(5))
+  if (num === 1) {
+    //Your party finds a small lake and decides to go for a swim. Unfortunately the lake was full of phirranas
+  } else if (num === 2) {
+    //You find a small bunny and decide to keep it. The bunny bites CharacterName. CharacterName has gonorrhea
+  } else if (num === 3) {
+    //Your party is ambush, they hold you hostage and take some of your food
+    //food -= number
+    //days += number
+  } else if (num === 4) {
+    //Your wagon wheel broke, in the distance you hear Jesus Take The Wheel
+  } else if (num === 5){
+    //Some of your food rots because CharacterName wet themselves as they napped on it.
+    //food -= number 
   }
 }
 
