0a31195c47d6ed402839ac871e95980828fc8e1b
Nate Cottle and Reese Glasscock <nate@>
2018-06-19 13:44:29 -0700

    add validator for NaN and not a number for the storeBuy function.

--- js/scripts.js
+++ js/scripts.js
@@ -85,14 +85,14 @@ Wagon.prototype.turn = function() {
     char.illnessChecker() //reduces health if infected
     char.statusAdjuster() //updates status on screen based on health
   });
-  if (wagon.food > 0) {
-  wagon.food -= (wagon.characters.length * 5 )
-} else if (wagon.food <= 0) {
-  wagon.food = 0
-}
-  this.days += 1
-}
-// function for resting -- cure illness, gain some health
+    if (wagon.food > 0) {
+    wagon.food -= (wagon.characters.length * 5 )
+  } else if (wagon.food <= 0) {
+    wagon.food = 0
+  }
+    this.days += 1
+  }
+  // function for resting -- cure illness, gain some health
 Wagon.prototype.rest = function() {
   wagon.characters.forEach(function(char){
     char.illness.splice(0, 1)
@@ -104,7 +104,7 @@ Wagon.prototype.rest = function() {
   wagon.food -= (wagon.characters.length * 5 )
   this.days += 1
 }
-//event grabber
+  //event grabber
 Wagon.prototype.eventGrabber = function() {
   var num = Math.floor(Math.random() * Math.floor(100))
   if (num >= 80) {
@@ -121,7 +121,7 @@ Wagon.prototype.eventGrabber = function() {
     //call death event
   }
 }
-
+  //random positiveEvent
 function positiveEvent() {
   var num = Math.floor(Math.random() * Math.floor(5))
   var ranSupplyIncrease = Math.floor(Math.random() * (200 - 100) + 100)
@@ -143,7 +143,7 @@ function positiveEvent() {
     wagon.food += ranSupplyIncrease
   }
 }
-
+  //random neutralEvent
 function neutralEvent() {
   var num = Math.floor(Math.random() * Math.floor(5))
   if (num === 1) {
@@ -158,7 +158,7 @@ function neutralEvent() {
     $(".ongoing-events").prepend("A member of your party explores their sexuality with a neighbor boy. <br>")
   }
 }
-
+  //random negativeEvent
 function negativeEvent() {
   var num = Math.floor(Math.random() * Math.floor(5))
   var ranSupplyDecrease = Math.floor(Math.random() * (200 - 100) + 100)
@@ -257,23 +257,28 @@ Wagon.prototype.profession = function() {
 }
 
 function storeSubTotal(food) {
+  console.log(food);
   var total = (food * 0.2)
   return total.toFixed(2)
 }
 
 function storeBuy(food) {
-  wagon.food += food
-  wagon.money -= (food * 0.2)
-
-  var total = (food * 0.2)
-  return total
+    wagon.food += food
+    wagon.money -= (food * 0.2)
+    var total = (food * 0.2)
+    if (total == NaN || isNaN(total)) {
+      console.log(total);
+      $("#store").effect("shake", {times:3}, 700);
+    }
+    else {
+      $("#store").fadeOut(500);
+      $("#gameMainScreen").delay(500).fadeIn(500);
+      return total
+  }
 }
 
 function validateNames(profession, playerOne, playerTwo, playerThree, playerFour, playerFive) {
   if (profession === undefined || playerOne === "" || playerTwo === "" || playerThree === "" || playerFour === "" || playerFive === "") {
-    // for (var i = 0; i < 6; i++) {
-    //   $("#char" + i).effect("shake", {times:3}, 700);
-    // }
     $("#charNameInput").effect("shake", {times:3}, 700);
     $("#profession").effect("shake", {times:3}, 700)
   } else {
@@ -281,6 +286,12 @@ function validateNames(profession, playerOne, playerTwo, playerThree, playerFour
     $("#store").delay(500).fadeIn(500);
   }
 }
+
+// function validateStore(money){
+//   if (money === NaN || isNaN(money)) {
+//     $("#store").effect("shake", {times:3}, 700);
+//   }
+// }
 
 $(document).ready(function(){
   var x = 1;
@@ -336,8 +347,6 @@ $(document).ready(function(){
   });
 
   $("#storeBTN").click(function(){
-    $("#store").fadeOut(500);
-    $("#gameMainScreen").delay(500).fadeIn(500);
     var buyFood = parseInt($("#store input").val())
     storeBuy(buyFood)
     $('#wagon-food-remaining').text(wagon.food);
